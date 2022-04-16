ae93d545ea5acb4df3c5d2d9a2322998a268b309
SamhitaAchanta <sammy.achanta@gmail.com>
2022-04-16 16:05:48 -0500

    choose_story is done

--- server.py
+++ server.py
@@ -7,10 +7,15 @@ from random import randint
 RECV_BUFFER_SIZE = 2048
 
 def choose_story(c):
-    ## TODO
-    story_arr = ['test1', 'test2']
-    choice = randint(0, len(story_arr) - 1)
-    return story_arr[choice]
+    # ask client to choose a story
+    askClient = ("Choose a story (1), (2), (3): ")
+    c.send(askClient.encode())
+    choice1 = c.recv(RECV_BUFFER_SIZE).decode()
+    return choice1
+
+##    story_arr = ['test1', 'test2'] 
+##    choice = randint(0, len(story_arr) - 1) # randomly chooses a story
+##    return story_arr[choice]
 
 def get_prompts(title):
     ## TODO
@@ -29,12 +34,15 @@ def make_story(responses):
 
 def play_game(connection):
     ## pick story, send title
-    title = choose_story(connection)
-    connection.send(title.encode())
-    prompt_arr = get_prompts(title)
-    resp_arr = [send_prompt(p, connection) for p in prompt_arr]
+    title = choose_story(connection) 
+    connection.send(title.encode()) 
+    
+    prompt_arr = get_prompts(title) 
+    
+    resp_arr = [send_prompt(p, connection) for p in prompt_arr] 
     connection.send(str.encode('DONE'))
-    print(resp_arr)
+    print(resp_arr) 
+    
     story = make_story(resp_arr)
     connection.sendall(story.encode())
     connection.close()
@@ -48,15 +56,16 @@ def main():
     except error as e:
         print('error binding socket', str(e))
 
-    print('----- TCP Server is ready. ----- \n')
+    print('----- TCP Server is ready. ----- ')
 
     serverSocket.listen() #waits for TCP connection request
 
     while True:
         try:
             connection, addr = serverSocket.accept()
-            print('----- TCP Server accepted connection with ', addr[0], ':', str(addr[1]), ' -----')
+            print('----- TCP Server accepted connection with ', addr[0], ':', str(addr[1]), ' -----\n')
             start_new_thread(play_game, (connection, ))
+            print("~~ WELCOME TO MADLIBS ~~")
         except KeyboardInterrupt:
             serverSocket.close()
 
